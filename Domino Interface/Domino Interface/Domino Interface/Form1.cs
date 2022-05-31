bb55e636b3b25018084ec52f459524ca65515ffd
DBNumb <david.ars78@gmail.com>
2022-05-30 21:20:11 -0400

    Our domino interface

---
+++ Domino Interface/Domino Interface/Domino Interface/Form1.cs
@@ -0,0 +1,9 @@
+namespace Domino_Interface;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Domino Interface/Domino Interface/Domino Interface/Program.cs
@@ -0,0 +1,14 @@
+namespace Domino_Interface;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
