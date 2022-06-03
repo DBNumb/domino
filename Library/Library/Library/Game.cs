8081abd1a077acfeff7f8f8486d07a219fa76202
DBNumb <david.ars78@gmail.com>
2022-06-02 23:57:37 -0400

    Branch DM

---
+++ Library/Library/Library/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace Library;
+
+public class Game
+{
+    public Game(Player[] players)
+    {
+        Turns turn = new Turns(0, players.Length - 1);
+    }
+}
---
+++ Library/Library/Library/Token.cs
@@ -0,0 +1,13 @@
+﻿namespace Library;
+
+public class Token
+{
+    public int value1, value2;
+    public int score;
+    public Token(int value1, int value2)
+    {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.score = value1 + value2;
+    }
+}
