7c8773038fe6f15449cc9c34baea8461741d960f
DBNumb <david.ars78@gmail.com>
2022-06-03 12:42:03 -0400

    fixup! Added Tokens and tokens rules

---
+++ Library/Library/Library/Interfaces.cs
@@ -0,0 +1,6 @@
+﻿namespace Library;
+
+public interface Ifilter<T>
+{
+    public bool Apply(T x);
+}
---
+++ Library/Library/Library/RulesInterfaces.cs
@@ -0,0 +1,7 @@
+﻿namespace Library;
+
+public interface ITokenRule
+{
+    public Ifilter<Token> Check();
+    
+}
