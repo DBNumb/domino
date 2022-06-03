cb7e22edc496655959beef2c271409f0ea745f34
Link2233 <josedamiantadeoescarra@gmail.com>
2022-06-04 01:12:19 +0200

    players

--- Library/Library/Library/Player.cs
+++ Library/Library/Library/Player.cs
@@ -6,14 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    public class PosiblesJugadas: IFilterFichas
-    {
-        public Token[] posiblesjugadas { get; set; }
-        public bool Apply(Token[] fichasPlayer, Tuple<int, int> Estado_tablero)
-        {
-            throw new NotImplementedException();
-        }
-    }
+    
     public abstract class Player
     {
         //clase abstracta jugador
---
+++ Library/Library/Library/PlayerPD.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class PlayerPD : Player
+    {
+        //player protege data
+        public override Token Juega(Token[] posiblesjugadas)
+        {
+            Token jugada;
+            List<int> numerosDiferentes = new List<int>();
+
+            jugada = posiblesjugadas[0];//esto es para q no me marque error en el return....
+
+            for (int i = 0; i< posiblesjugadas.Length; i++) 
+            {
+                if (!numerosDiferentes.Contains(posiblesjugadas[i].values.Item1)) 
+                {
+                    numerosDiferentes.Add(posiblesjugadas[i].values.Item1);
+                }
+                if (!numerosDiferentes.Contains(posiblesjugadas[i].values.Item2))
+                {
+                    numerosDiferentes.Add(posiblesjugadas[i].values.Item2);
+                }
+            }
+
+            int[] repeticionesPorNumero = new int[numerosDiferentes.Count];
+
+            for (int i = 0; i< numerosDiferentes.Count; i++) 
+            {
+                int count = 0;
+                for (int j =0; j< posiblesjugadas.Length; j++) 
+                {
+                    if (numerosDiferentes[i] == posiblesjugadas[j].values.Item1 || numerosDiferentes[i] == posiblesjugadas[j].values.Item2) 
+                    {
+                        count++;
+                    }
+                }
+                repeticionesPorNumero[i] = count;
+            }
+
+            int data = repeticionesPorNumero.Max();
+
+            for (int i = 0; i < posiblesjugadas.Length; i++) 
+            {
+                if (!(posiblesjugadas[i].values.Item1 == data || posiblesjugadas[i].values.Item2 == data)) 
+                {
+                    jugada = posiblesjugadas[i];
+                    break;
+                }                
+            }
+            return jugada;
+        }
+    }
+}
