bb752a7ab69e7f25b11935e3de8618a34effd07a
DBNumb <david.ars78@gmail.com>
2022-06-05 01:48:58 -0400

    A lot of fixes

---
+++ Library/Library/Library/Board.cs
@@ -0,0 +1,27 @@
+﻿namespace Library;
+
+public class Board
+{
+    public List<Token> board;
+
+    public Board()
+    {
+        board = new List<Token>();
+        
+    }
+
+    public int CountBoard(int value)
+    {
+        int count = 0;
+        foreach (var token in board)
+        {
+            if (value == token.Item1 || value == token.Item2)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+  
+}
---
+++ Library/Library/Library/Log.cs
@@ -0,0 +1,11 @@
+﻿namespace Library;
+
+public static class Log
+{
+    public static List<string> moves;
+
+    public static void Register(Token play, int player)
+    {
+        moves.Add($"Jugador {player} jugó la ficha [{play.Item1},{play.Item2}]");
+    }
+}
