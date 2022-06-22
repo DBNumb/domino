a4f9821fc1825bf87e6a86230a5a551131b9a1fb
DBNumb <david.ars78@gmail.com>
2022-06-22 00:05:11 -0400

    first interface edit

--- Domino Interface/Domino Interface/Domino Interface/Settings.Designer.cs
+++ Domino Interface/Domino Interface/Domino Interface/Settings.Designer.cs
@@ -28,81 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
-            this.listBox3 = new System.Windows.Forms.ListBox();
-            this.listBox4 = new System.Windows.Forms.ListBox();
             this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownRandomplayer = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownPDPlayer = new System.Windows.Forms.NumericUpDown();
+            this.SettingsStartButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownPBG = new System.Windows.Forms.NumericUpDown();
+            this.domainUpDownTurnRule = new System.Windows.Forms.DomainUpDown();
+            this.domainUpDownToken = new System.Windows.Forms.DomainUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.domainUpDownWinRule = new System.Windows.Forms.DomainUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRandomplayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPDPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(47, 175);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(77, 34);
-            this.listBox1.TabIndex = 0;
-            // 
-            // listBox2
-            // 
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 15;
-            this.listBox2.Location = new System.Drawing.Point(190, 175);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(77, 34);
-            this.listBox2.TabIndex = 1;
-            // 
-            // listBox3
-            // 
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 15;
-            this.listBox3.Location = new System.Drawing.Point(348, 175);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(77, 34);
-            this.listBox3.TabIndex = 2;
-            // 
-            // listBox4
-            // 
-            this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 15;
-            this.listBox4.Location = new System.Drawing.Point(502, 175);
-            this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(77, 34);
-            this.listBox4.TabIndex = 3;
             // 
             // listBox5
             // 
             this.listBox5.FormattingEnabled = true;
-            this.listBox5.ItemHeight = 15;
-            this.listBox5.Location = new System.Drawing.Point(657, 175);
+            this.listBox5.ItemHeight = 30;
+            this.listBox5.Location = new System.Drawing.Point(1126, 350);
+            this.listBox5.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(77, 34);
+            this.listBox5.Size = new System.Drawing.Size(129, 64);
             this.listBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(11, 627);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cantidad de jugadores:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(10, 707);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 32);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Player Random:";
+            // 
+            // numericUpDownRandomplayer
+            // 
+            this.numericUpDownRandomplayer.Location = new System.Drawing.Point(217, 707);
+            this.numericUpDownRandomplayer.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownRandomplayer.Name = "numericUpDownRandomplayer";
+            this.numericUpDownRandomplayer.Size = new System.Drawing.Size(69, 35);
+            this.numericUpDownRandomplayer.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(10, 761);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 32);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Player P.Data";
+            // 
+            // numericUpDownPDPlayer
+            // 
+            this.numericUpDownPDPlayer.Location = new System.Drawing.Point(217, 758);
+            this.numericUpDownPDPlayer.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownPDPlayer.Name = "numericUpDownPDPlayer";
+            this.numericUpDownPDPlayer.Size = new System.Drawing.Size(69, 35);
+            this.numericUpDownPDPlayer.TabIndex = 10;
+            // 
+            // SettingsStartButton
+            // 
+            this.SettingsStartButton.Location = new System.Drawing.Point(565, 12);
+            this.SettingsStartButton.Name = "SettingsStartButton";
+            this.SettingsStartButton.Size = new System.Drawing.Size(161, 78);
+            this.SettingsStartButton.TabIndex = 11;
+            this.SettingsStartButton.Text = "button1";
+            this.SettingsStartButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(11, 815);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 32);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Player Bota.G";
+            // 
+            // numericUpDownPBG
+            // 
+            this.numericUpDownPBG.Location = new System.Drawing.Point(217, 815);
+            this.numericUpDownPBG.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownPBG.Name = "numericUpDownPBG";
+            this.numericUpDownPBG.Size = new System.Drawing.Size(69, 35);
+            this.numericUpDownPBG.TabIndex = 13;
+            // 
+            // domainUpDownTurnRule
+            // 
+            this.domainUpDownTurnRule.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.domainUpDownTurnRule.Items.Add("1-Turno Default");
+            this.domainUpDownTurnRule.Items.Add("2-Turno Reloj");
+            this.domainUpDownTurnRule.Location = new System.Drawing.Point(342, 365);
+            this.domainUpDownTurnRule.Name = "domainUpDownTurnRule";
+            this.domainUpDownTurnRule.Size = new System.Drawing.Size(210, 35);
+            this.domainUpDownTurnRule.TabIndex = 14;
+            this.domainUpDownTurnRule.Text = "Regla de turno";
+            // 
+            // domainUpDownToken
+            // 
+            this.domainUpDownToken.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.domainUpDownToken.Location = new System.Drawing.Point(597, 365);
+            this.domainUpDownToken.Name = "domainUpDownToken";
+            this.domainUpDownToken.Size = new System.Drawing.Size(210, 35);
+            this.domainUpDownToken.TabIndex = 15;
+            this.domainUpDownToken.Text = "Regla de ficha";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(11, 853);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(430, 38);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Cantidad de fichas por jugador:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(447, 858);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 35);
+            this.numericUpDown1.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(11, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(320, 38);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Valor de ficha máximo:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(20, 472);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(92, 35);
+            this.numericUpDown2.TabIndex = 19;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // domainUpDownWinRule
+            // 
+            this.domainUpDownWinRule.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.domainUpDownWinRule.Location = new System.Drawing.Point(829, 365);
+            this.domainUpDownWinRule.Name = "domainUpDownWinRule";
+            this.domainUpDownWinRule.Size = new System.Drawing.Size(210, 35);
+            this.domainUpDownWinRule.TabIndex = 20;
+            this.domainUpDownWinRule.Text = "Regla de ganar";
             // 
             // Settings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1371, 900);
+            this.Controls.Add(this.domainUpDownWinRule);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.domainUpDownToken);
+            this.Controls.Add(this.domainUpDownTurnRule);
+            this.Controls.Add(this.numericUpDownPBG);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.SettingsStartButton);
+            this.Controls.Add(this.numericUpDownPDPlayer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDownRandomplayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox5);
-            this.Controls.Add(this.listBox4);
-            this.Controls.Add(this.listBox3);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "Settings";
             this.Text = "Settings";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRandomplayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPDPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private ListBox listBox1;
-        private ListBox listBox2;
-        private ListBox listBox3;
-        private ListBox listBox4;
         private ListBox listBox5;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDownRandomplayer;
+        private Label label3;
+        private NumericUpDown numericUpDownPDPlayer;
+        private Button SettingsStartButton;
+        private Label label4;
+        private NumericUpDown numericUpDownPBG;
+        private DomainUpDown domainUpDownTurnRule;
+        private DomainUpDown domainUpDownToken;
+        private Label label5;
+        private NumericUpDown numericUpDown1;
+        private Label label6;
+        private NumericUpDown numericUpDown2;
+        private DomainUpDown domainUpDownWinRule;
     }
 }
