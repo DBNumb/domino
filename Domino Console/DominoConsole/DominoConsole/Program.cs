0482eacd1e75d790b0a9582219040de51c71b658
DBNumb <david.ars78@gmail.com>
2022-07-09 01:42:42 -0400

    FirstConsoleSteps

--- Domino Console/DominoConsole/DominoConsole/Program.cs
+++ Domino Console/DominoConsole/DominoConsole/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Threading.Channels;
 
-Console.WriteLine("Hello, World!");
+namespace DominoConsole;
+public static class Program
+{
+    public static void Main(string[] args)
+    {  
+        bool menuout = false;
+        Action<string> WritewholeLine = s => Console.WriteLine(s);
+        Action<string> Write = s => Console.Write(s); 
+        WritewholeLine("Elija qué variaciónes quiere adicionar al juego en caso de qué no elija alguna\nse usaran la clásica ");
+        WritewholeLine("1- Regla de Turnos");
+        WritewholeLine("2- Regla de fichas");
+        WritewholeLine("3- Cantidad y Tipos de jugadores");
+        WritewholeLine("4- Regla de ganar: ");
+        WritewholeLine("10- Continuar: ");
+        WritewholeLine("11- Salir: ");
+        while (!menuout)
+        {
+            
+        }
+    }
+}
+
+
+
